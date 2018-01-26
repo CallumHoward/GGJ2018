1e43026738c28668b3e5d7b6c28634b74bc4fc71
Cong Xu <congusbongus@gmail.com>
2018-01-26 22:09:59 +1100

    Corralling demo scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        Direction = Camera.main.transform.TransformDirection(Direction);
-        Direction.y = 0f;
-        gameObject.transform.position = (transform.position + Direction * Time.deltaTime);
+		Vector3 Direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, Input.GetAxisRaw ("Vertical"));
+		Direction = Camera.main.transform.TransformDirection (Direction);
+		Direction.y = 0f;
+		gameObject.transform.position = (transform.position + Direction * Time.deltaTime);
+	}
+}
