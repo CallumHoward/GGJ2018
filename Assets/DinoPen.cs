d8e9a1daad9261add09cc62cd1d72372931288e7
Cong Xu <congusbongus@gmail.com>
2018-01-27 10:30:17 +1100

    DinoPen prefab files

---
+++ Assets/DinoPen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoPen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == "Raptor") {
+			Debug.Log ("Got a raptor");
+		}
+	}
+}
