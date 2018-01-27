6e5e1d1bb17f14983542072a7b442c5a976631e3
Cong Xu <congusbongus@gmail.com>
2018-01-27 17:58:28 +1100

    Open/close gate with g key

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour {
+
+	Animation anim;
+	public float AnimationSpeed;
+	bool open;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+		anim ["LatchOpen"].layer = 1;
+		open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("g")) {
+			PlayAnimation ("LatchOpen", open);
+			PlayAnimation ("GateOpen", open);
+			open = !open;
+		}
+	}
+
+	void PlayAnimation(string name, bool reverse) {
+		anim.clip = anim.GetClip(name);
+		anim [name].speed = AnimationSpeed * (reverse ? -1f : 1f);
+		if (reverse && anim [name].time == 0) {
+			anim [name].time = anim [name].length;
+		}
+		anim.Play ();
+	}
+}
