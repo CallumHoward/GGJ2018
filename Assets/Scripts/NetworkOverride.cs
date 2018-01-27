bdebf38363638b9828a4d2ed04e636d81e9c6d26
GTGalaxi <gtgalaxiyt@gmail.com>
2018-01-27 14:01:10 +1100

    asd

--- Assets/Scripts/NetworkOverride.cs
+++ Assets/Scripts/NetworkOverride.cs
@@ -5,25 +5,32 @@ using UnityEngine.Networking;
 
 public class NetworkOverride : NetworkManager {
 
+    
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
         base.OnServerAddPlayer(conn, playerControllerId);
         if (conn.playerControllers.Count > 0)
         {
-            //foreach(NetworkClient nc in NetworkClient.allClients[].c)
-            print(NetworkServer.connections.Count);
-            print(NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetInstanceID());
-            print(NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject);
-            NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponent<PlayerController>()._PLAYER = (PLAYER)NetworkServer.connections.Count - 1;
-            //print(conn.playerControllers[0].gameObject.GetInstanceID());
-            int counter = NetworkServer.connections.Count;
-            foreach (PlayerController pc in FindObjectsOfType<PlayerController>())
-            {
-                //print(pc.GetInstanceID());
-                //pc.gameObject.GetComponentInChildren<Renderer>().material.color = pc.Test();
-                NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponentInChildren<Renderer>().material.color = NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponent<PlayerController>().Test();
-            }
-            
+
+            Tester();
+        }
+    }
+
+    [ClientRpc]
+    public void Tester()
+    {
+        //foreach(NetworkClient nc in NetworkClient.allClients[].c)
+        print(NetworkServer.connections.Count);
+        print(NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetInstanceID());
+        print(NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject);
+        NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponent<PlayerController>()._PLAYER = (PLAYER)NetworkServer.connections.Count - 1;
+        //print(conn.playerControllers[0].gameObject.GetInstanceID());
+        int counter = NetworkServer.connections.Count;
+        foreach (PlayerController pc in FindObjectsOfType<PlayerController>())
+        {
+            //print(pc.GetInstanceID());
+            //pc.gameObject.GetComponentInChildren<Renderer>().material.color = pc.Test();
+            NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponentInChildren<Renderer>().material.color = NetworkServer.connections[NetworkServer.connections.Count - 1].playerControllers[0].gameObject.GetComponent<PlayerController>().Test();
         }
     }
     
