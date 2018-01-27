3a01b82eac0d325ca68e01636f9ca1d5a349d162
Cong Xu <congusbongus@gmail.com>
2018-01-27 16:26:52 +1100

    Add gates

--- Assets/Scripts/DinoPen.cs
+++ Assets/Scripts/DinoPen.cs
@@ -7,7 +7,9 @@ public class DinoPen : MonoBehaviour {
 	HashSet<string> dinoNames;
 	public GameObject FencePole;
 	public GameObject FenceRail;
+	public GameObject Gate;
 	public float FENCE_SECTION_LENGTH_MAX;
+	public int NUM_GATES;
 
 	// Use this for initialization
 	void Start () {
@@ -17,23 +19,43 @@ public class DinoPen : MonoBehaviour {
 
 	void SpawnFence() {
 		List<Vector3> polePositions = GetPolePositions ();
+		// Place a few gates
+		HashSet<int> gateIndices = new HashSet<int>();
+		for (int i = 0; i < NUM_GATES; i++) {
+			gateIndices.Add((int)(polePositions.Count * (i + 0.5f) / NUM_GATES));
+		}
 		// Spawn fence poles and rails at pole positions
 		for (int i = 0; i < polePositions.Count; i++) {
 			Vector3 position = polePositions [i];
-			Instantiate(FencePole, transform.position + position, Quaternion.identity);
+			Vector3 positionNext = polePositions [(i + 1) % polePositions.Count];
+			if (gateIndices.Contains(i)) {
+				// Place gate
+				PlaceRailOrGate(Gate, position, positionNext);
+			} else {
+				if (!gateIndices.Contains ((i - 1 + polePositions.Count) % polePositions.Count)) {
+					Instantiate (FencePole, transform.position + position, Quaternion.identity);
+				}
 
-			if (Random.value < 0.8f) {
-				Vector3 positionNext = polePositions [(i + 1) % polePositions.Count];
-				Vector3 railPosition = Vector3.Lerp (position, positionNext, 0.5f);
-				float yAngle = Vector3.SignedAngle (Vector3.forward, positionNext - position, Vector3.up);
-				GameObject rail = Instantiate (
-					                  FenceRail, transform.position + railPosition, Quaternion.AngleAxis (yAngle, Vector3.up));
-				float length = FenceRail.GetComponent<Renderer> ().bounds.size.z;
-				Vector3 scale = FenceRail.transform.localScale;
-				scale.z = Vector3.Distance (position, positionNext) * scale.z / length;
-				rail.transform.localScale = scale;
+				// Place railing
+				PlaceRailOrGate(FenceRail, position, positionNext);
 			}
 		}
+	}
+
+	void PlaceRailOrGate(GameObject parent, Vector3 position, Vector3 positionNext) {
+		Vector3 midPosition = Vector3.Lerp (position, positionNext, 0.5f);
+		float yAngle = Vector3.SignedAngle (Vector3.forward, positionNext - position, Vector3.up);
+		GameObject obj = Instantiate (
+			parent, transform.position + midPosition, Quaternion.AngleAxis (yAngle, Vector3.up));
+		if (parent.GetComponent<Renderer> () == null) {
+			// TODO: gate placement
+			return;
+		}
+		Bounds bounds = parent.GetComponent<Renderer> ().bounds;
+		float length = bounds.size.z;
+		Vector3 scale = parent.transform.localScale;
+		scale.z = Vector3.Distance (position, positionNext) * scale.z / length;
+		obj.transform.localScale = scale;
 	}
 
 	List<Vector3> GetPolePositions() {
